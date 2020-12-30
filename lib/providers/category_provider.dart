304ad636a30c5f636b8e00764bc7e71366881346
Alan Rabello <alan.rabello@serpro.gov.br>
2020-12-30 11:43:39 -0300

    Add clear and changes the structure to singleton

--- lib/providers/category_provider.dart
+++ lib/providers/category_provider.dart
@@ -5,15 +5,32 @@ import 'package:ar_list/providers/json_provider.dart';
 
 class CategoryProvider {
   static const fileName = 'category.json';
-  static Future<Set<Category>> readCategories() async {
-    final jsonString = await JsonProvider.readFile(CategoryProvider.fileName);
-    return (json.decode(jsonString) as List)
-        .map((i) => Category.fromJson(i))
-        .toSet();
+  static final CategoryProvider instance = CategoryProvider._();
+  Set<Category> data;
+
+  CategoryProvider._() {
+    this.read();
   }
 
-  static void writeCategories(Set<Category> categories) {
+  Future<Set<Category>> read() async {
+    try {
+      final jsonString = await JsonProvider.readFile(CategoryProvider.fileName);
+      this.data = (json.decode(jsonString) as List)
+          .map((i) => Category.fromJson(i))
+          .toSet();
+    } catch (error) {
+      this.data = <Category>{};
+      this.write();
+    }
+    return this.data;
+  }
+
+  void write() {
     JsonProvider.saveFile(
-        CategoryProvider.fileName, json.encode(categories.toList()));
+        CategoryProvider.fileName, json.encode(this.data.toList()));
+  }
+
+  void clear() {
+    JsonProvider.deleteFile(CategoryProvider.fileName);
   }
 }
