e9a5f7033bf40d9df32825e6cd5ffd884a65a3ca
Alan Rabello <alan.rabello@serpro.gov.br>
2020-12-30 17:31:15 -0300

    Generalize the cone variable names

--- test/category_provider_test.dart
+++ test/category_provider_test.dart
@@ -5,7 +5,7 @@ import 'package:ar_list/providers/category_provider.dart';
 void main() {
   test('Categories must be saved', () async {
     // Create some categories
-    CategoryProvider cp = CategoryProvider.instance;
+    CategoryProvider provider = CategoryProvider.instance;
     final Set<Category> categories = <Category>{};
 
     categories.add(Category('Teste 1'));
@@ -14,17 +14,17 @@ void main() {
     categories.add(Category('Teste 3'));
     categories.add(Category('Teste 1'));
 
-    cp.data = categories;
-    cp.write();
+    provider.data = categories;
+    await provider.write();
 
-    final Set<Category> fromFile = await cp.read();
+    final Set<Category> fromFile = await provider.read();
 
     expect(fromFile.length, 4);
 
     fromFile.toList()
       ..sort((a, b) => a.compareTo(b))
       ..forEach((element) => print(element.toJson()));
-  }, skip: true);
+  }, skip: false);
 
   test('Categories are comparable', () {
     Category c1 = Category('Teste 1');
