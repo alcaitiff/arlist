0d9492c555f9a49e2db6bdaa47939a4d1b057e84
Alan Rabello <alan.rabello@serpro.gov.br>
2021-01-04 11:59:01 -0300

    Basic code for the form

--- lib/screens/create/components/body.dart
+++ lib/screens/create/components/body.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class Body extends StatelessWidget {
+  final GlobalKey<FormState> _formKey;
+
+  Body(this._formKey);
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text('Go back!'),
-      ),
-    );
+    return SafeArea(
+        top: false,
+        bottom: false,
+        child: new Form(
+            key: _formKey,
+            child: new ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              children: <Widget>[
+                new TextFormField(
+                  decoration: InputDecoration(
+                    hintText: AppLocalizations.of(context).list_name_hint,
+                    labelText: AppLocalizations.of(context).list_name,
+                  ),
+                ),
+                new Container(
+                    child: new RaisedButton(
+                  child: const Text('Submit'),
+                  onPressed: null,
+                )),
+              ],
+            )));
   }
 }
