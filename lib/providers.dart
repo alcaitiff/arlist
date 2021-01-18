f48fc9c984bf4b8e59e76a0ce0b8d90d5b3bd018
Alan Rabello <alan.rabello@serpro.gov.br>
2021-01-18 11:59:10 -0300

    Providers for entries

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,6 +1,8 @@
 import 'package:ar_list/business/ShopItem/notifier.dart';
 import 'package:ar_list/business/ShopList/notifier.dart';
 import 'package:ar_list/models/shop_item.dart';
+import 'package:ar_list/models/shop_list.dart';
+import 'package:ar_list/models/shop_list_entry.dart';
 import 'package:ar_list/repositories/shop_item_repository.dart';
 import 'package:ar_list/repositories/shop_list_repository.dart';
 import 'package:hooks_riverpod/all.dart';
@@ -27,6 +29,40 @@ enum SortType {
   alpha,
   inversedAlpha,
 }
+
+final currentList = StateProvider<ShopList>((ref) => ShopList(''));
+
+final entryFilter = StateProvider((ref) => '');
+final entrySortType = StateProvider((ref) => SortType.alpha);
+
+final filteredEntries = Provider<List<ShopListEntry>>((ref) {
+  final filter = ref.watch(entryFilter);
+  final items = ref.watch(currentList).state.items;
+  final sortType = ref.watch(entrySortType);
+
+  if (items != null && items.length > 0) {
+    final result = items
+        .where((e) =>
+            e.item.name.toLowerCase().contains(filter.state.toLowerCase()))
+        .toList();
+    if (sortType.state == SortType.alpha) {
+      result.sort((a, b) {
+        return a.item.name
+            .toLowerCase()
+            .compareTo(b.item.name.toString().toLowerCase());
+      });
+    } else if (sortType.state == SortType.inversedAlpha) {
+      result.sort((a, b) {
+        return b.item.name
+            .toLowerCase()
+            .compareTo(a.item.name.toString().toLowerCase());
+      });
+    }
+    return result;
+  } else {
+    return [];
+  }
+});
 
 final shopItemSortType = StateProvider((ref) => SortType.alpha);
 
